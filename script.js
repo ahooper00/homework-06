19e6899275f4e6c0772f304573902fa3bda65fc8
Amelia Hooper <ajhooper97@gmail.com>
2021-09-27 13:59:21 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const searchFormEl = document.querySelector("#search-form");
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const searchInput = document.querySelector("#search-input").value;
+
+    if(!searchInput) {
+        console.error("You need a search input value!");
+        return;
+    }
+    const queryString = './search-results.html?=' + searchInput;
+
+    location.assign(queryString)
+}
+
+searchFormEl.addEventListener('submit', handleFormSubmit);
